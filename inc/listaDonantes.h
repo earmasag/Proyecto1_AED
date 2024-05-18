2eadd77727aa07b606fd3653bb1e7b7a895aa512
Uchichaxd <142691207+Uchichaxd@users.noreply.github.com>
2024-05-18 16:54:40 -0400

    Update listaDonantes.h

--- inc/listaDonantes.h
+++ inc/listaDonantes.h
@@ -1,5 +1,9 @@
+#ifndef listaDonantes
+#define listaDonantes
 #include "donante.h"
 
 int donanteVacia(Donante *cabeza);
 Donante *agregarDonante(Donante *cabeza, Donante *nuevo);
 Donante *buscar_cedula(Donante *cabeza,int buscar_cedula);
+
+#endif
