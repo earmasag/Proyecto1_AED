073482abb675c0c6f3905a18647d9bfd03ac2815
Uchichaxd <142691207+Uchichaxd@users.noreply.github.com>
2024-05-18 16:57:12 -0400

    Create txt_manager.h

---
+++ inc/txt_manager.h
@@ -0,0 +1,14 @@
+#ifndef txt_manager
+#define txt_manager
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "donante.h"
+#include "listaDonantes.h"
+#include "donaciones.h"
+
+Donante *cargarListaDonantes(Donante *HEAD);
+Donaciones *cargarListaDonaciones(Donaciones *HEAD);
+
+#endif
